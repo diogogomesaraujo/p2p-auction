fd0c92cc86adfb869023a98be496d16b9586c3ab
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2026-03-20 18:15:57 +0000

    implemented the 4 kademlia idiomatic rpcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,57 @@
 use libp2p::{
     Multiaddr, PeerId, StreamProtocol, Swarm, SwarmBuilder,
     futures::StreamExt,
+    identify,
     identity::Keypair,
     kad::{
-        self, AddProviderOk, Behaviour, Event, GetProvidersOk, GetRecordOk, Mode, PeerRecord,
-        PutRecordOk, QueryResult, Record, store::MemoryStore,
+        self, GetRecordOk, Mode, PeerRecord, PutRecordOk, QueryResult, Record, store::MemoryStore,
     },
-    noise,
-    swarm::SwarmEvent,
+    noise, ping,
+    swarm::{NetworkBehaviour, SwarmEvent},
     tcp, yamux,
 };
 use std::{error::Error, time::Duration};
 use tokio::io::{AsyncBufReadExt, BufReader, Stdin};
 
+//simmilar to example from https://docs.rs/libp2p/latest/libp2p/swarm/trait.NetworkBehaviour.html
+#[derive(NetworkBehaviour)]
+#[behaviour(to_swarm = "MyBehaviourEvent")]
+pub struct MyBehaviour {
+    pub kad: kad::Behaviour<MemoryStore>,
+    pub ping: ping::Behaviour,
+    pub identify: identify::Behaviour,
+}
+
+#[derive(Debug)]
+pub enum MyBehaviourEvent {
+    Kad(kad::Event),
+    Ping(ping::Event),
+    Identify(identify::Event),
+}
+
+impl From<kad::Event> for MyBehaviourEvent {
+    fn from(event: kad::Event) -> Self {
+        Self::Kad(event)
+    }
+}
+
+impl From<ping::Event> for MyBehaviourEvent {
+    fn from(event: ping::Event) -> Self {
+        Self::Ping(event)
+    }
+}
+
+impl From<identify::Event> for MyBehaviourEvent {
+    fn from(event: identify::Event) -> Self {
+        Self::Identify(event)
+    }
+}
+
 pub async fn kad_instance_init(
     ipfs_proto_name: StreamProtocol,
     key: Keypair,
     bootstrap_nodes: &[&str],
-) -> Result<Swarm<Behaviour<MemoryStore>>, Box<dyn Error + Send + Sync>> {
+) -> Result<Swarm<MyBehaviour>, Box<dyn Error + Send + Sync>> {
     let mut swarm = SwarmBuilder::with_existing_identity(key)
         .with_tokio()
         .with_tcp(
@@ -27,28 +61,49 @@ pub async fn kad_instance_init(
         )?
         .with_dns()?
         .with_behaviour(|key| {
-            let mut cfg = kad::Config::new(ipfs_proto_name);
-            cfg.set_query_timeout(Duration::from_mins(1));
+            let local_id = key.public().to_peer_id();
+
+            let mut kad_cfg = kad::Config::new(ipfs_proto_name.clone());
+            kad_cfg.set_query_timeout(Duration::from_secs(60));
+
             let store = kad::store::MemoryStore::new(key.public().to_peer_id());
-            kad::Behaviour::with_config(key.public().to_peer_id(), store, cfg)
+
+            let kad = kad::Behaviour::with_config(local_id, store, kad_cfg);
+
+            let ping = ping::Behaviour::new(
+                ping::Config::new()
+                    .with_interval(Duration::from_secs(10))
+                    .with_timeout(Duration::from_secs(3)),
+            );
+
+            let identify = identify::Behaviour::new(identify::Config::new(
+                ipfs_proto_name.to_string(),
+                key.public(),
+            ));
+
+            Ok(MyBehaviour {
+                kad,
+                ping,
+                identify,
+            })
         })?
         .build();
 
     for node in bootstrap_nodes {
-        swarm.behaviour_mut().add_address(
+        swarm.behaviour_mut().kad.add_address(
             &node.parse::<PeerId>()?,
             "/dnsaddr/bootstrap.libp2p.io".parse()?,
         );
     }
 
-    swarm.behaviour_mut().set_mode(Some(Mode::Server));
+    swarm.behaviour_mut().kad.set_mode(Some(Mode::Server));
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     Ok(swarm)
 }
 
 pub async fn kad_run(
-    swarm: &mut Swarm<Behaviour<MemoryStore>>,
+    swarm: &mut Swarm<MyBehaviour>,
     buffer_reader: BufReader<Stdin>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut lines = buffer_reader.lines();
@@ -57,145 +112,137 @@ pub async fn kad_run(
             Ok(Some(line)) = lines.next_line() => {
                 let mut args = line.split_whitespace();
                 match args.next() {
-                    Some("ADD_ADDRESS") => {
-                        let peer_id = match args.next() {
-                            Some(peer_id) => peer_id.parse::<PeerId>()?,
-                            None => {
-                                eprintln!("Expected peer id");
-                                continue;
-                            }
-                        };
-                        let address = match args.next() {
-                            Some(address) => address.parse::<Multiaddr>()?,
-                            None => {
-                                eprintln!("Expected address");
-                                continue;
-                            }
-                        };
-                        swarm.behaviour_mut().add_address(&peer_id, address);
-                    }
                     Some("PING") => {
-                        let (address_str, address) = match args.next() {
-                            Some(address) => (address, address.parse::<Multiaddr>()?),
+                        let address = match args.next() {
+                            Some(addr) => addr.parse::<Multiaddr>()?,
                             None => {
                                 eprintln!("Expected address");
                                 continue;
                             }
                         };
+
                         swarm.dial(address)?;
-                        println!("Dialed {address_str}")
                     }
-                    Some("GET_VALUE") => {
-                        let key = {
-                            match args.next() {
-                                Some(key) => kad::RecordKey::new(&key),
-                                None => {
-                                    eprintln!("Expected key");
-                                    continue;
-                                }
+
+                    Some("STORE") => {
+                        let key = match args.next() {
+                            Some(key) => kad::RecordKey::new(&key),
+                            None => {
+                                eprintln!("Expected key");
+                                continue;
                             }
                         };
-                        swarm.behaviour_mut().get_record(key);
-                    }
-                    Some("GET_PROVIDERS") => {
-                        let key = {
-                            match args.next() {
-                                Some(key) => kad::RecordKey::new(&key),
-                                None => {
-                                    eprintln!("Expected key");
-                                    continue;
-                                }
+
+                        let value = match args.next() {
+                            Some(value) => value.as_bytes().to_vec(),
+                            None => {
+                                eprintln!("Expected value");
+                                continue;
                             }
                         };
-                        swarm.behaviour_mut().get_providers(key);
-                    }
-                    Some("PUT_VALUE") => {
-                        let key = {
-                            match args.next() {
-                                Some(key) => kad::RecordKey::new(&key),
-                                None => {
-                                    eprintln!("Expected key");
-                                    continue;
-                                }
-                            }
-                        };
-                        let value = {
-                            match args.next() {
-                                Some(value) => value.as_bytes().to_vec(),
-                                None => {
-                                    eprintln!("Expected value");
-                                    continue;
-                                }
-                            }
-                        };
+
                         let record = kad::Record {
                             key,
                             value,
                             publisher: None,
                             expires: None,
                         };
+
                         swarm.behaviour_mut()
+                            .kad
                             .put_record(record, kad::Quorum::One)?;
                     }
-                    Some("PUT_PROVIDER") => {
-                        let key = {
-                            match args.next() {
-                                Some(key) => kad::RecordKey::new(&key),
-                                None => {
-                                    eprintln!("Expected key");
-                                    continue;
-                                }
+
+                    Some("FIND_NODE") => {
+                        let peer = match args.next() {
+                            Some(peer) => peer.parse::<PeerId>()?,
+                            None => {
+                                eprintln!("Expected peer id");
+                                continue;
                             }
                         };
 
-                        swarm.behaviour_mut()
-                            .start_providing(key)?;
+                        swarm.behaviour_mut().kad.get_closest_peers(peer);
                     }
+
+                    Some("FIND_VALUE") => {
+                        let key = match args.next() {
+                            Some(key) => kad::RecordKey::new(&key),
+                            None => {
+                                eprintln!("Expected key");
+                                continue;
+                            }
+                        };
+
+                        swarm.behaviour_mut().kad.get_record(key);
+                    }
+
                     _ => {}
                 }
             }
+
             event = swarm.select_next_some() => {
                 match event {
                     SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
-                    SwarmEvent::Behaviour(Event::RoutingUpdated { peer, is_new_peer, addresses, ..}) => {
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Kad(kad::Event::RoutingUpdated { peer, is_new_peer, addresses, ..})) => {
                         println!("Routing table updated: {peer:?}, is it a new peer? {is_new_peer}, addresses: {addresses:?}");
                     },
-                    SwarmEvent::Behaviour(Event::OutboundQueryProgressed { result, .. }) => {
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Kad(kad::Event::OutboundQueryProgressed { result, .. })) => {
                         match result {
+                            QueryResult::GetClosestPeers(Ok(ok)) => {
+                                println!("FIND_NODE result: {:?}", ok.peers);
+                            }
+
+                            QueryResult::GetClosestPeers(Err(e)) => {
+                                eprintln!("FIND_NODE error: {}", e);
+                            }
+
                             QueryResult::GetRecord(Ok(GetRecordOk::FoundRecord(
                                 PeerRecord { record: Record { key, value, .. }, .. }
                             ))) => {
-                                println!("Received GET_VALUE successful response: key: {:?}, value: {}", key, String::from_utf8(value)?);
+                                println!("Received FIND_VALUE successful response: key: {:?}, value: {}", key, String::from_utf8(value)?);
                             }
                             QueryResult::GetRecord(Err(e)) => {
-                                eprintln!("Received GET_VALUE error: {e}");
+                                eprintln!("Received FIND_VALUE error: {e}");
                             }
 
                             QueryResult::PutRecord(Ok(PutRecordOk{key})) => {
-                                println!("Received PUT_VALUE successful response: key: {:?}", key);
+                                println!("Received STORE successful response: key: {:?}", key);
                             }
                             QueryResult::PutRecord(Err(e)) => {
-                                eprintln!("Received PUT_VALUE error: {e}");
+                                eprintln!("Received STORE error: {e}");
                             }
 
-                            QueryResult::GetProviders(Ok(GetProvidersOk::FoundProviders { key, providers, .. })) => {
-                                providers.iter().for_each(|provider| println!("Received GET_PROVIDER successful message: provider: {:?}, key: {:?}", provider, key));
-                            }
-                            QueryResult::GetProviders(Err(e)) => {
-                                eprintln!("Received GET_PROVIDER error: {e}");
-                            }
+                            // QueryResult::GetProviders(Ok(GetProvidersOk::FoundProviders { key, providers, .. })) => {
+                            //     providers.iter().for_each(|provider| println!("Received GET_PROVIDER successful message: provider: {:?}, key: {:?}", provider, key));
+                            // }
+                            // QueryResult::GetProviders(Err(e)) => {
+                            //     eprintln!("Received GET_PROVIDER error: {e}");
+                            // }
 
-                            QueryResult::StartProviding(Ok(AddProviderOk{key})) => {
-                                println!("Received PUT_PROVIDER successful message: key: {:?}", key);
-                            }
-                            QueryResult::StartProviding(Err(e)) => {
-                                eprintln!("Received GET_PROVIDER error: {e}");
-                            }
+                            // QueryResult::StartProviding(Ok(AddProviderOk{key})) => {
+                            //     println!("Received PUT_PROVIDER successful message: key: {:?}", key);
+                            // }
+                            // QueryResult::StartProviding(Err(e)) => {
+                            //     eprintln!("Received GET_PROVIDER error: {e}");
+                            // }
 
                             _ => {}
                         }
                     },
-                    SwarmEvent::Behaviour(event) => println!("{event:?}"),
+
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Ping(event)) => {
+                            println!("PING event: {event:?}");
+                    },
+
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Identify(identify::Event::Received { peer_id, info, .. })) => {
+                        for addr in info.listen_addrs {
+                            swarm.behaviour_mut().kad.add_address(&peer_id, addr);
+                        }
+
+                        let _ = swarm.behaviour_mut().kad.bootstrap();
+                    },
+
                     _ => {}
                 }
             }
