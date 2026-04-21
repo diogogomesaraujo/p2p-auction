fd041e2f12351f8e4f81eee6af6ffafd97fff9e6
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-04-21 11:46:14 +0100

    tests

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -7,6 +7,7 @@ type HashFunction = Blake2b512;
 // https://towardsdev.com/the-proof-of-work-pow-mechanism-in-blockchain-6a49196cab75
 // https://www.jmeiners.com/tiny-blockchain/
 // https://en.bitcoin.it/wiki/Protocol_documentation#Block_Headers
+// https://hackernoon.com/rusty-chains-a-basic-blockchain-implementation-written-in-pure-rust-gk2m3uri
 
 pub mod hash {
     use crate::blockchain::HashFunction;
@@ -16,6 +17,23 @@ pub mod hash {
         h.update(data.as_bytes());
         let bytes = h.finalize().to_vec();
         hex::encode(bytes)
+    }
+
+    #[cfg(test)]
+    pub mod test {
+        use crate::blockchain::hash::hash;
+        use blake2::{Blake2b512, Digest};
+
+        #[test]
+        fn test_hash() {
+            let to_hash = "I am not in danger, Skyler. I am the danger.";
+            let hashed = hash(Blake2b512::new(), to_hash);
+
+            assert_eq!(
+                hashed,
+                "3a141d45dea6b8af5bab5f942d88f3c0d48edcda84fac341d821d13d65896e2a7d5a8ec921da654301e72db33631fd94963e064056172f4d970a77625aa7ed93"
+            );
+        }
     }
 }
 
@@ -30,14 +48,15 @@ pub mod pow {
         pub difficulty: u32,
     }
 
-    pub fn mine(pow: &ProofOfWork, hasher: HashFunction) -> (String, u32) {
+    // todo: refactor
+    pub fn mine(pow: &ProofOfWork, hasher: &HashFunction) -> (String, u32) {
         let prefix = [0..pow.difficulty]
             .iter()
             .fold(String::new(), |acc, _| [acc, String::from("0")].join(""));
 
         fn mine_rec(
             pow: &ProofOfWork,
-            hasher: HashFunction,
+            hasher: &HashFunction,
             nonce: u32,
             prefix: &str,
         ) -> (String, u32) {
@@ -59,6 +78,7 @@ pub mod pow {
     }
 }
 
+#[derive(Debug)]
 pub struct Block {
     pub index: u32,
     pub previous_hash: String,
@@ -74,7 +94,7 @@ impl Block {
         previous_hash: String,
         data: String,
         difficulty: u32,
-        hasher: HashFunction,
+        hasher: &HashFunction,
     ) -> Result<Self, Box<dyn Error + Send + Sync>> {
         let p = pow::ProofOfWork { data, difficulty };
         let (h, nonce) = pow::mine(&p, hasher);
@@ -89,6 +109,7 @@ impl Block {
     }
 }
 
+#[derive(Debug)]
 pub struct Blockchain {
     pub blocks: Vec<Block>,
     pub difficulty: u32,
@@ -97,7 +118,7 @@ pub struct Blockchain {
 impl Blockchain {
     pub fn new(
         difficulty: u32,
-        hasher: HashFunction,
+        hasher: &HashFunction,
     ) -> Result<Self, Box<dyn Error + Send + Sync>> {
         let genesis_block = Block::new(
             0,
@@ -115,7 +136,7 @@ impl Blockchain {
     pub fn add_block(
         &mut self,
         data: &str,
-        hasher: HashFunction,
+        hasher: &HashFunction,
     ) -> Result<(), Box<dyn Error + Send + Sync>> {
         let previous_block = match self.blocks.last() {
             Some(pb) => pb,
@@ -128,6 +149,29 @@ impl Blockchain {
             self.difficulty,
             hasher,
         )?);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use std::error::Error;
+
+    use crate::blockchain::Blockchain;
+    use blake2::{Blake2b512, Digest};
+
+    #[test]
+    fn test_blockchain() -> Result<(), Box<dyn Error + Send + Sync>> {
+        let h = Blake2b512::new();
+        let mut blockchain = Blockchain::new(u32::MAX, &h)?;
+
+        blockchain.add_block("First Block", &h)?;
+        blockchain.add_block("Second Block", &h)?;
+        blockchain.add_block("Third Block", &h)?;
+        blockchain.add_block("Fourth Block", &h)?;
+
+        println!("{:?}", blockchain);
+
         Ok(())
     }
 }
